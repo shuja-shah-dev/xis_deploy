2a1a3e7ae6555eda3e4d407ecd183ea668bb1292
Ahmad Hasham <ahmadhasham2001@gmail.com>
2024-02-24 14:38:08 +0500

    video converted AVC level 3.1

--- components/HeroSection.js
+++ components/HeroSection.js
@@ -99,7 +99,7 @@ const HeroSection = () => {
           loop
           className="w-full  h-full object-cover z-50"
         >
-          <source src="/hero-video.mp4" type="video/mp4" />
+          <source src="/output.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
       </div>
