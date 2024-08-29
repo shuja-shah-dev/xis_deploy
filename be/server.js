1ae5e9a311e2afcc68ec9528a882458a60d20fe6
Ahmad Hasham <ahmadhasham2001@gmail.com>
2024-08-29 14:27:10 +0500

    route fixed

--- be/server.js
+++ be/server.js
@@ -26,7 +26,7 @@ app.use(bodyParser.json());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
-app.post("/contact", postcontact);
+app.use("/contact", postcontact);
 app.post("/founder", founderRoute);
 app.use("/", blogRoutes);
 app.use("/", jobRoutes);
