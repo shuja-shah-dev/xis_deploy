2e958c3850564782fd850f742ff43884454cdf7e
Ahmad Hasham <ahmadhasham2001@gmail.com>
2024-10-17 18:11:51 +0500

    blogs rendering fixed

--- pages/blogpost/[slug].js
+++ pages/blogpost/[slug].js
@@ -40,31 +40,54 @@ const Heading = ({ children, level }) => {
     marginTop: "6px",
     marginBottom: "6px",
   };
+  // switch (level) {
+  //   case 1:
+  //     return (
+  //       <Typography variant="h2" style={headingStyles}>
+  //         {children}
+  //       </Typography>
+  //     );
+  //   case 2:
+  //     return (
+  //       <Typography variant="h3" style={headingStyles}>
+  //         {children}
+  //       </Typography>
+  //     );
+  //   case 3:
+  //     return (
+  //       <Typography variant="h4" style={headingStyles}>
+  //         {children}
+  //       </Typography>
+  //     );
+  //   case 4:
+  //     return (
+  //       <Typography variant="h5" style={headingStyles}>
+  //         {children}
+  //       </Typography>
+  //     );
+  //     case 5:
+  //       return (
+  //         <Typography variant="h6" style={headingStyles}>
+  //         {children}
+  //       </Typography>
+  //       );
+  // }
+  
   switch (level) {
     case 1:
-      return (
-        <Typography variant="h3" style={headingStyles}>
-          {children}
-        </Typography>
-      );
+      return <Typography variant="h1">{children}</Typography>
     case 2:
-      return (
-        <Typography variant="h4" style={headingStyles}>
-          {children}
-        </Typography>
-      );
+      return <Typography variant="h2">{children}</Typography>
     case 3:
-      return (
-        <Typography variant="h5" style={headingStyles}>
-          {children}
-        </Typography>
-      );
+      return <Typography variant="h3">{children}</Typography>
     case 4:
-      return (
-        <Typography variant="h6" style={headingStyles}>
-          {children}
-        </Typography>
-      );
+      return <Typography variant="h4">{children}</Typography>
+    case 5:
+      return <Typography variant="h5">{children}</Typography>
+    case 6:
+      return <Typography variant="h6">{children}</Typography>
+    default:
+      return <Typography variant="h1">{children}</Typography>
   }
 };
 
@@ -338,6 +361,11 @@ const Slug = ({ blog }) => {
                         paragraph: ({ children }) => <p className="break-words">{children}</p>,
                         list: ListComponent,
                         heading: Heading,
+                      
+                      }}
+                      modifiers={{
+                        bold: ({ children }) => <strong>{children}</strong>,
+                        italic: ({ children }) => <span className="italic">{children}</span>,
                       }}
                     />
                   </div>
